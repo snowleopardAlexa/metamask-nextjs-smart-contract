174b0af833443fbcd565797ca5d38540cfdc53a6
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-08 20:35:41 -0400

    add hardhat simple storage - fake blockchain, add contract address, add abi

--- index.js
+++ index.js
@@ -12,6 +12,96 @@
       // contract ABI (blueprint to interact with a contract)
       // function
       // node connection
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+      const abi =  [
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_favoriteNumber",
+              "type": "uint256"
+            }
+          ],
+          "name": "addPerson",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "nameToFavoriteNumber",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "people",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "favoriteNumber",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "retrieve",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_favoriteNumber",
+              "type": "uint256"
+            }
+          ],
+          "name": "store",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ]
    }
 
    // export functions
