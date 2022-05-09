931012f94265b9ffff120b695901d2c5a0dd492a
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-08 21:41:27 -0400

    add button

--- nextjs-ethers/pages/index.js
+++ nextjs-ethers/pages/index.js
@@ -1,20 +1,29 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers'
 // connect to metamask
 // execute function
 
 export default function Home() {
 
   const [isConnected, setIsConnect] = useState(false)
+  const [provider, setProvider] = useState()
   
   async function connect() {
     if (typeof window.ethereum !== "undefined") {
       try {
         await ethereum.request({ method: "eth_requestAccounts"})
+        setIsConnected(true)
+        let connectedProvider = new ethers.providers.Web3Provider(
+          window.ethereum
+        )
+        setSigner(connectedProvider.getSigner())
       } catch(e) {
         console.log(e)
       }
+    } else {
+      setIsConnected(false)
     }
   }
 
@@ -27,8 +36,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>hello alexa</h1>
-
-    </div>
+    <button onClick={() => connect()}>
+      Connect!
+    </button>
+    
+   </div>
   )
 }
